8be8cffc71187f1449a8f2ed94868179ab8cf59a
Jacob Kline <jacobk513@gmail.com>
2024-11-03 15:06:24 -0500

    adding state variable lab topics

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,9 +1,13 @@
+import ArrayStateVariable from "./ArrayStateVariable";
 import BooleanStateVariables from "./BooleanStateVariables";
 import ClickEvent from "./ClickEvent";
 import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariable";
 import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import StringStateVariables from "./StringStateVariables";
 
 export default function Lab4() {
 
@@ -20,5 +24,9 @@ export default function Lab4() {
             <EventObject />
             <Counter />
             <BooleanStateVariables />
+            <StringStateVariables />
+            <DateStateVariable />
+            <ObjectStateVariable />
+            <ArrayStateVariable />
         </div>);
 }
