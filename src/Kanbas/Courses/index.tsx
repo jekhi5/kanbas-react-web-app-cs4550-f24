ed77f45c572011ef28184b7538f0751d6304c530
Jacob Kline <jacobk513@gmail.com>
2024-11-04 17:40:01 -0500

    added state to courses dashboard

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,12 +6,11 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
-import * as db from "../Database";
 
-export default function Courses() {
+export default function Courses({ courses }: { courses: any[]; }) {
     const { cid } = useParams();
     const { pathname } = useLocation();
-    const course = db.default.courses.find((course) => course._id === cid);
+    const course = courses.find((c) => c._id === cid);
 
     return (
         <div id="wd-courses">
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,7 +4,40 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
+import * as db from "./Database";
+import { useState } from "react";
 export default function Kanbas() {
+
+    const [courses, setCourses] = useState<any[]>(db.default.courses);
+    const [course, setCourse] = useState<any>({
+        _id: "0", name: "New Course", number: "New Number",
+        startDate: "2023-09-10", endDate: "2023-12-15",
+        image: "/images/reactjs.jpg", description: "New Description"
+    });
+    const addNewCourse = () => {
+        const newCourse = {
+            ...course,
+            _id: new Date().getTime().toString()
+        };
+        setCourses([...courses, newCourse]);
+    };
+
+    const deleteCourse = (courseId: string) => {
+        setCourses(courses.filter((course) => course._id !== courseId));
+    };
+
+    const updateCourse = () => {
+        setCourses(
+            courses.map((c) => {
+                if (c._id === course._id) {
+                    return course;
+                } else {
+                    return c;
+                }
+            })
+        );
+    };
+
     return (
         <div id="wd-kanbas">
             <KanbasNavigation />
@@ -12,9 +45,22 @@ export default function Kanbas() {
                 <Routes>
                     <Route path="/" element={<Navigate to="/Kanbas/Account" />} />
                     <Route path="/Account/*" element={<Account />} />
-                    <Route path="/Dashboard" element={<Dashboard />} />
-                    <Route path="/Courses" element={<Dashboard />} />
-                    <Route path="/Courses/:cid/*" element={<Courses />} />
+                    <Route path="/Dashboard" element={<Dashboard
+                        courses={courses}
+                        course={course}
+                        setCourse={setCourse}
+                        addNewCourse={addNewCourse}
+                        deleteCourse={deleteCourse}
+                        updateCourse={updateCourse} />} />
+                    <Route path="/Courses" element={<Dashboard
+                        courses={courses}
+                        course={course}
+                        setCourse={setCourse}
+                        addNewCourse={addNewCourse}
+                        deleteCourse={deleteCourse}
+                        updateCourse={updateCourse} />
+                    } />
+                    <Route path="/Courses/:cid/*" element={<Courses courses={courses} />} />
                     <Route path="/Calendar" element={<h1>Calendar</h1>} />
                     <Route path="/Inbox" element={<h1>Inbox</h1>} />
                 </Routes>
